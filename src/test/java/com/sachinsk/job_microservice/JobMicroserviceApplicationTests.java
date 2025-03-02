a6bcfb942f8f714f9fb7e8b13fe641e6765806d8
Sachin <sachinkaushikofficial@gmail.com>
2025-03-02 14:43:17 +0530

    Initial commit

---
+++ src/test/java/com/sachinsk/job_microservice/JobMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinsk.job_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
